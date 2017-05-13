5fe6340b5bd2751caeda7f5236e12f0786282af7
daniel.bedo <danielbedo@gmail.com>
2017-05-13 18:35:05 +0200

    statsusservice unit test for not decodeable json

--- src/test/scala/com/github/danielbedo/riot4s/service/status/RiotStatusServiceComponentSpec.scala
+++ src/test/scala/com/github/danielbedo/riot4s/service/status/RiotStatusServiceComponentSpec.scala
@@ -6,7 +6,7 @@ import cats.implicits._
 import com.github.danielbedo.riot4s.util.ApiTestUtils
 import cats.data.EitherT
 import com.github.danielbedo.ApiErrors
-import com.github.danielbedo.ApiErrors.ApiError
+import com.github.danielbedo.ApiErrors.{ApiError, UnparseableJson}
 import com.github.danielbedo.riot4s.Regions
 import com.github.danielbedo.riot4s.http.LeagueApiComponent
 
@@ -31,6 +31,18 @@ class RiotStatusServiceComponentSpec extends FlatSpec with Matchers with MockFac
     val shardResponse = Await.result(shardFuture, 1 seconds)
 
     assert(shardResponse.isRight)
+  }
+
+  "RiotStatsService" should "respond with UnparseableJson if response from API not decodeable" in {
+    val services = new testFixture {
+      val invalidShardJsonResponse = EitherT.right[Future, ApiError, String](Future("{noone expected this invalid json}"))
+      (leagueApi.get _).when(statusService.getUrlForShardData(Regions.EUW), *).returns(invalidShardJsonResponse)
+    }
+
+    val shardFuture = services.statusService.getShardData(Regions.EUW).value
+    val shardResponse = Await.result(shardFuture, 1 seconds)
+
+    assert(shardResponse == Left(UnparseableJson))
   }
 
 }
