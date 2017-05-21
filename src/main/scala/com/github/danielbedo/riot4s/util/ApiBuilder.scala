2905c4042d6428d5660a0ea5bb1c1aa89c05a127
daniel.bedo <danielbedo@gmail.com>
2017-05-21 12:33:20 +0200

    fixes #6

---
+++ src/main/scala/com/github/danielbedo/riot4s/util/ApiBuilder.scala
@@ -0,0 +1,25 @@
+package com.github.danielbedo.riot4s.util
+
+import akka.actor.ActorSystem
+import com.github.danielbedo.riot4s.ActorSystemProvider
+import com.github.danielbedo.riot4s.cache.GuavaServiceCacheComponent
+import com.github.danielbedo.riot4s.http.DefaultLeagueApiComponent
+import com.github.danielbedo.riot4s.service.statsv13.RiotStatsServiceComponent
+import com.github.danielbedo.riot4s.service.status.RiotStatusServiceComponent
+import com.github.danielbedo.riot4s.service.summoner.RiotSummonerServiceComponent
+
+case class ApiBuilder(key: String)(implicit as: ActorSystem) {
+
+  def build() = {
+    new DefaultLeagueApiComponent
+      with ActorSystemProvider
+      with RiotStatusServiceComponent
+      with RiotSummonerServiceComponent
+      with RiotStatsServiceComponent
+      with GuavaServiceCacheComponent {
+      override val actorSystem = as
+      override val apiKey: String = key
+    }
+  }
+
+}
