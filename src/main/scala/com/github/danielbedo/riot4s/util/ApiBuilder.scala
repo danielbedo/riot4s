f8caf98e4995e7cbe525c912aedef382d9837778
daniel.bedo <danielbedo@gmail.com>
2017-05-21 13:36:39 +0200

    no cache for simple api builder

--- src/main/scala/com/github/danielbedo/riot4s/util/ApiBuilder.scala
+++ src/main/scala/com/github/danielbedo/riot4s/util/ApiBuilder.scala
@@ -2,7 +2,7 @@ package com.github.danielbedo.riot4s.util
 
 import akka.actor.ActorSystem
 import com.github.danielbedo.riot4s.ActorSystemProvider
-import com.github.danielbedo.riot4s.cache.GuavaServiceCacheComponent
+import com.github.danielbedo.riot4s.cache.{NoopCacheComponent}
 import com.github.danielbedo.riot4s.http.DefaultLeagueApiComponent
 import com.github.danielbedo.riot4s.service.statsv13.RiotStatsServiceComponent
 import com.github.danielbedo.riot4s.service.status.RiotStatusServiceComponent
@@ -16,7 +16,7 @@ case class ApiBuilder(key: String)(implicit as: ActorSystem) {
       with RiotStatusServiceComponent
       with RiotSummonerServiceComponent
       with RiotStatsServiceComponent
-      with GuavaServiceCacheComponent {
+      with NoopCacheComponent {
       override val actorSystem = as
       override val apiKey: String = key
     }
